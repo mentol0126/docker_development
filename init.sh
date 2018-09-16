27a59d6e17c93e811283fce48c4ca6a4b942fd4e
ya310 <mentol310@gmail.com>
2018-09-16 10:55:20 +0900

    初期化スクリプトから phpMyAdmin 入るようにする

--- init.sh
+++ init.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-mkdir hello
-mkdir db/mysql_data
-mkdir -p html/info
-touch html/info/index.php
-echo '<?php phpinfo();' > html/info/index.php
+if [ ! -e hello ]; then
+  mkdir hello
+  mkdir db/mysql_data
+  mkdir -p html/info
+  touch html/info/index.php
+  echo '<?php phpinfo();' > html/info/index.php
+fi
+
+if [ ! -e html/phpMyAdmin ]; then
+  curl -L -O https://files.phpmyadmin.net/phpMyAdmin/4.8.3/phpMyAdmin-4.8.3-all-languages.zip
+  unzip phpMyAdmin-4.8.3-all-languages.zip
+  mv phpMyAdmin-4.8.3-all-languages html/phpMyAdmin
+  rm -f phpMyAdmin-4.8.3-all-languages.zip
+fi
+
